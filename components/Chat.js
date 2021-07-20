f22eb73b5fccdbb1b0297ee905054b264d96a3ae
pstaal <peter.staal@gmail.com>
2021-07-20 12:42:20 +0200

    Add gifted chat code to chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 
 const styles = StyleSheet.create({
@@ -8,6 +9,36 @@ const styles = StyleSheet.create({
 
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    });
+  }
+
   render() {
     let name = this.props.route.params.name;
     let color = this.props.route.params.color;
@@ -15,7 +46,11 @@ export default class Chat extends React.Component {
 
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: color }}>
-        <Text style={{color: '#fff'}}>Let's start chatting!</Text>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{_id: 1,}}
+        />
       </View>
     )
   }
