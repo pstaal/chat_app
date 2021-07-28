e3e550d91a315a050d7442fd569380c906342466
pstaal <peter.staal@gmail.com>
2021-07-28 14:59:23 +0200

    debugging

--- components/Chat.js
+++ components/Chat.js
@@ -34,7 +34,7 @@ export default class Chat extends React.Component {
     // ]);
   }
 
-  async getMessages = () => {
+  async getMessages() {
     let messages = '';
     try {
       messages = await AsyncStorage.getItem('messages') || [];
